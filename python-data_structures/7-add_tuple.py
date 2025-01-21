af94dad7c906f9ddd7e1857f583db794c75bc7cf
aaymenb <9954@holbertonstudents.com>
2025-01-21 16:05:12 +0100

    add 2 tuples

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    # Gestion du tuple a
     if len(tuple_a) == 0:
-        
+
         tuple_a = (0, 0)
     elif len(tuple_a) == 1:
 
         tuple_a = (tuple_a[0], 0)
 
-    # Gestion du tuple b
 
     if len(tuple_b) == 0:
 
@@ -16,7 +14,6 @@ def add_tuple(tuple_a=(), tuple_b=()):
     elif len(tuple_b) == 1:
         tuple_b = (tuple_b[0], 0)
 
-    # Addition des éléments des tuples
     final_tuple = (int(tuple_a[0]) + int(tuple_b[0]),
                    int(tuple_a[1]) + int(tuple_b[1]))
 
