dd1f86679aee553a184c41b2320678d2f930dbed
aaymenb <9954@holbertonstudents.com>
2025-02-06 16:10:31 +0100

    Abstract Classes and Interfaces

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -1,14 +1,37 @@
 from abc import ABC, abstractmethod
+from math import pi
 
-class Animal(ABC):
+class Shape(ABC):
     @abstractmethod
-    def sound(self):
+    def area(self):
+        pass
+    @abstractmethod
+    def perimeter(self):
         pass
 
-class Dog(Animal):
-    def sound(self):
-        return ("Bark")
-    
-class Cat(Animal):
-    def sound(self):
-        return "Meow"
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+    def area(self):
+        if self.radius < 0:
+            return 0
+        return pi * self.radius ** 2
+    def perimeter(self):
+        if self.radius < 0:
+            return 0
+        return 2 * pi * self.radius
+
+class Rectangle(Shape):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+    def area(self):
+        return self.width * self.height
+    def perimeter(self):
+        return 2 * (self.width + self.height)
+
+def shape_info(shape):
+    area = shape.area()
+    perimeter = shape.perimeter()
+    print(f"Area: {area}")
+    print(f"Perimeter: {perimeter}")
