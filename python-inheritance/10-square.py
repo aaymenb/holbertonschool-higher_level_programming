7de7c884b6463b1cca66c0d9f7c662579d8c6fab
aaymenb <9954@holbertonstudents.com>
2025-02-06 15:01:03 +0100

    Python - Inheritance

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Class a square follow by rectangle"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Write a class Square that inherits from Rectangle"""
+    def __init__(self, size):
+        """Function that innitialize a square"""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(self.__size, self.__size)
+
+    def area(self):
+        """method must be implemented an area"""
+        return self.__size ** 2
